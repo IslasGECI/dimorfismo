447ca4f8dcfeffab95f2ccec5ab6df6a4c8bcf63
Andrea <a348644@uabc.edu.mx>
2020-04-24 13:25:55 -0700

    Aplica guía de estilo al código

--- src/functions/my_functions.R
+++ src/functions/my_functions.R
@@ -1,8 +1,8 @@
-write_a_word <- function(parametro){
+write_a_word <- function(parametro) {
     return(parametro)
 }
 
-resolve_riddle <- function(parametro_1, parametro_2){
-    sol = (parametro_1 * parametro_2) + parametro_1
-    return(sol)
+resolve_riddle <- function(parametro_1, parametro_2) {
+    answer <- (parametro_1 * parametro_2) + parametro_1
+    return(answer)
 }
