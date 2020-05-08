c725b767b46165ed9d1ab8ace89076a003ed5d3a
Andrea <a348644@uabc.edu.mx>
2020-05-08 10:39:36 -0700

    Resuelve primera prueba para revisar las dependencias

--- src/functions/dimorfismo.R
+++ src/functions/dimorfismo.R
@@ -6,3 +6,10 @@ resolve_riddle <- function(parameter_1, parameter_2) {
     answer <- (parameter_1 * parameter_2) + parameter_1
     return(answer)
 }
+
+is_library <- function(library) {
+    installed <- installed.packages()
+    packages <- installed[ , 1]
+    answer <- is.element(library, packages)
+    return(answer)
+}
