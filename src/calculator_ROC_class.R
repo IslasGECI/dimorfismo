8ee5ba0365a5f1a6b68492326d145ec228f6671b
Andrea <a348644@uabc.edu.mx>
2020-05-14 14:05:53 -0700

    Aplica espacios en los programas clases

--- src/calculator_ROC_class.R
+++ src/calculator_ROC_class.R
@@ -7,7 +7,7 @@ ROC <- R6Class("ROC",
     initialize = function() {
     },
     
-    getBestThresholdAndError = function(datos){
+    getBestThresholdAndError = function(datos) {
         self$rOC <- private$calculateROC(datos)
         self$rOC <- private$addCriterion(self$rOC)
         mejoresUmbrales <- self$rOC %>% 
@@ -15,10 +15,10 @@ ROC <- R6Class("ROC",
             summarize(umbral = median(umbrales),
                       error = median(pError)) %>%
             arrange(criterio)
-        return(mejoresUmbrales[1,c(2,3)])
+        return(mejoresUmbrales[1, c(2,3)])
     },
 
-    calculateError = function(datos, umbral){
+    calculateError = function(datos, umbral) {
         confusion <- private$calculateConfusion(datos, umbral)
         confusion <- private$addMissing(confusion)
         pError <- private$calculateErrorRate(confusion)
@@ -26,16 +26,16 @@ ROC <- R6Class("ROC",
     }
   ),
   private = list(
-    classifyAnswer = function(renglon, umbral){
+    classifyAnswer = function(renglon, umbral) {
         respuesta <- ifelse(renglon$prob > umbral, 1, 0)
         falsoOVerdadero <- ifelse(renglon$y_test == respuesta, 'V', 'F')
-        clasificacion <- paste0(falsoOVerdadero,respuesta)
+        clasificacion <- paste0(falsoOVerdadero, respuesta)
         return(clasificacion)
     },
       
-    calculateConfusion = function(datos, umbral){
+    calculateConfusion = function(datos, umbral) {
         confusion <- datos %>% 
-        mutate(clasificacion = private$classifyAnswer(.,umbral)) %>% 
+        mutate(clasificacion=private$classifyAnswer(., umbral)) %>% 
         group_by(clasificacion) %>%
         summarize(N = n())
         return(confusion)
@@ -48,44 +48,44 @@ ROC <- R6Class("ROC",
         confusion <- confusion %>% merge(base, by = "clasificacion", all = T) %>% 
             mutate(N = N.x + N.y) %>% 
             mutate(N = ifelse(is.na(N), 0, N)) %>%
-            select(c(1,4))
+            select(c(1, 4))
         return(confusion)
     },
       
-    calculateTPR = function(confusion){
-        tpr <- confusion[[4,2]]/(confusion[[4,2]]+confusion[[1,2]])
+    calculateTPR = function(confusion) {
+        tpr <- confusion[[4, 2]] / (confusion[[4, 2]] + confusion[[1, 2]])
         return(tpr)
     },
       
-    calculateFPR = function(confusion){
-        fpr <- confusion[[2,2]]/(confusion[[3,2]]+confusion[[2,2]])
+    calculateFPR = function(confusion) {
+        fpr <- confusion[[2, 2]] / (confusion[[3, 2]] + confusion[[2, 2]])
         return(fpr)
     },
       
-    calculateErrorRate = function(confusion){
-        error <- ((confusion[[2,2]]+confusion[[1,2]])/sum(confusion[,2]))*100
+    calculateErrorRate = function(confusion) {
+        error <- ((confusion[[2, 2]] + confusion[[1,2]]) / sum(confusion[, 2])) * 100
         return(error)
     },
       
-    calculateROC = function(datos){
+    calculateROC = function(datos) {
         umbrales <- 1:100 * 0.01
         fPR <- c()
         tPR <- c()
         pError <- c()
-        for(umbral in umbrales){
+        for (umbral in umbrales) {
             confusion <- private$calculateConfusion(datos, umbral)
             confusion <- private$addMissing(confusion)
             fPR <- append(fPR, private$calculateFPR(confusion))
             tPR <- append(tPR, private$calculateTPR(confusion))
             pError <- append(pError, private$calculateErrorRate(confusion))
         }                     
-        ROC <- data.frame(umbrales,tPR, fPR, pError)
+        ROC <- data.frame(umbrales, tPR, fPR, pError)
         return(ROC)
     },
       
     addCriterion = function(rOC) {
         rOC <- rOC %>% 
-            mutate(criterio = sqrt((fPR)^2+(1-tPR)^2)) 
+            mutate(criterio = sqrt((fPR)^2 + (1 - tPR)^2)) 
         return(rOC)
     }
   )
--- src/dimorphism_model_class.R
+++ src/dimorphism_model_class.R
@@ -2,36 +2,40 @@ library(R6)
 
 ModeloDimorfismo <- R6Class("ModeloDimorfismo",
   public = list(
-    loadParameters = function(JSONpath){
+    loadParameters = function(JSONpath) {
       #escribir codigo para cargar los parametros
       # Importa parametrosModelo
-      jsonParametros <- rjson::fromJSON(file=JSONpath)
+      jsonParametros <- rjson::fromJSON(file = JSONpath)
       private$ParametrosNormalizacion <- jsonParametros$parametrosNormalizacion
       private$ParametrosModelo <- jsonParametros$parametrosModelo
     },
-    predict = function(TablaDatosMorfometricos){
+    predict = function(TablaDatosMorfometricos) {
       # escribir codigo para predecir sexo
       z <- private$getValue("(Intercept)")
       iVarible <- 1
-      for (variable in private$ParametrosModelo){
+      for (variable in private$ParametrosModelo) {
         if (variable$Variables != "(Intercept)") {
-          columna <- TablaDatosMorfometricos[,variable$Variables, with=FALSE]
+          columna <- TablaDatosMorfometricos[, variable$Variables, with = FALSE]
           minimo <- as.numeric(private$ParametrosNormalizacion$valorMinimo[variable$Variables])
           maximo <- as.numeric(private$ParametrosNormalizacion$valorMaximo[variable$Variables])
-          normalize <- function(columna) (columna - minimo)/(maximo-minimo)
-          ColumnaNormalizada <- as.data.frame(apply(columna,2,normalize))
+
+          normalize <- function(columna) {
+            (columna - minimo)/(maximo - minimo)
+          }
+
+          ColumnaNormalizada <- as.data.frame(apply(columna, 2, normalize))
           z <- z + ColumnaNormalizada*private$getValue(variable$Variables)
-          iVarible <- iVarible+1
+          iVarible <- iVarible + 1
         }
       }
-      probabilidad <-  1 / (1+exp(-z))
+      probabilidad <- 1 / (1 + exp(-z))
       colnames(probabilidad)<- "probability"
       return(probabilidad)
     },
-    getVariablesNames = function(){
+    getVariablesNames = function() {
       variablesModelo <- c()
       
-      for (variable in private$ParametrosModelo){
+      for (variable in private$ParametrosModelo) {
         if (variable$Variables != "(Intercept)") {
           variablesModelo <- c(variablesModelo, variable$Variables)
         }
@@ -43,7 +47,7 @@ ModeloDimorfismo <- R6Class("ModeloDimorfismo",
     ParametrosModelo = NULL, 
     ParametrosNormalizacion = NULL, 
     getValue = function(nombreVariable) {
-      for (variable in private$ParametrosModelo){
+      for (variable in private$ParametrosModelo) {
         if (variable$Variables == nombreVariable) {
           return(variable$Estimate)
         }
