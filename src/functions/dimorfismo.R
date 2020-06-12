7a5dd081080c236b47d00730aa474e228a1134d9
Andrea <a348644@uabc.edu.mx>
2020-06-12 13:17:16 -0700

    Elimina ejercicios con fines didácticos de pruebas de caja negra

--- src/functions/dimorfismo.R
+++
@@ -1,8 +0,0 @@
-write_a_word <- function(parameter) {
-    return(parameter)
-}
-
-resolve_riddle <- function(parameter_1, parameter_2) {
-    answer <- (parameter_1 * parameter_2) + parameter_1
-    return(answer)
-}
