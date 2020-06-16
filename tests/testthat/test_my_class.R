49042cacb60df620d9e50fab97f40e78fff7d92f
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-06-16 15:11:49 -0700

    Mueve test_my_class.R al repo ejemplo_para_andrea

--- tests/testthat/test_my_class.R
+++
@@ -1,8 +0,0 @@
-source("../../src/functions/dimorfismo.R")
-Mi_Primera_Clase <- My_First_Class$new() 
-test_that("Que tengan los requerimientos para POO",
-    {
-        expect_true(is_library("tidyverse"))
-        expect_true(is_element("myFirstClass", class(Mi_Primera_Clase)))
-    }
-)
