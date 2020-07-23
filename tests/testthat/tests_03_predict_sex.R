b50699f3a7c505b22e8800aef3e2224a48a43f02
Andrea <a348644@uabc.edu.mx>
2020-07-22 18:03:22 -0700

    Agrega prueba de valor umbral

--- tests/testthat/tests_03_predict_sex.R
+++ tests/testthat/tests_03_predict_sex.R
@@ -1,6 +1,7 @@
 library(testthat)
 
 setwd("/workdir/")
+source("tests/data/03_predict_sex_data_tests.R")
 source("src/03_predict_sex.R")
 
 json_correct_data_path <- ("tests/data/logistic_model_parameters_tests.json")
@@ -11,5 +12,11 @@ json_data <- rjson::fromJSON(file = json_data_path)
 test_that("Los resultados generados del código son correctos:",
     {
         expect_equal(json_data, json_correct_data)
+    }
+)
+
+test_that("El valor umbral es correcto:",
+    {
+        expect_equal(threshold, correct_threshold)
     }
 )
