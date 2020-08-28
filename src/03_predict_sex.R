32f94760b9130f7c027ebc35af5d8d5722cb70fa
Andrea <a348644@uabc.edu.mx>
2020-08-27 22:37:46 -0700

    Elimina muchas líneas no necesarias en 03_predict_sex.R

--- src/03_predict_sex.R
+++ src/03_predict_sex.R
@@ -16,33 +16,9 @@ males <- c()
 for (i_albatross in 1:n_rows_data) {
   data <- csv_data[i_albatross, ]
   for (i_row in 1:n_rows_table) {
-    auxiliar_coefficients_table <- imported_table[i_row, 1:5]
-    auxiliar_coefficients_table <- data.frame(
-      data.table::melt(auxiliar_coefficients_table),
-      row.names = colnames(auxiliar_coefficients_table)
-    )
-    colnames(auxiliar_coefficients_table) <- c("Variables", "Estimate")
     threshold <- as.numeric(imported_table[i_row, 7])
-    max_auxiliar_normalized_table <- imported_table[i_row, 14:17]
-    colnames(max_auxiliar_normalized_table) <- rownames(auxiliar_coefficients_table[2:5, ])
-    min_auxiliar_normalized_table <- imported_table[i_row, 9:12]
-    colnames(min_auxiliar_normalized_table) <- rownames(auxiliar_coefficients_table[2:5, ])
-    normalization_parameters <- list(
-      minimum_value = as.list(min_auxiliar_normalized_table),
-      maximum_value = as.list(max_auxiliar_normalized_table)
-    )
-    list_normalization_parameters <- list(
-      normalization_parameters = normalization_parameters,
-      model_parameters = auxiliar_coefficients_table
-    )
-
-    readr::write_lines(
-      jsonlite::toJSON(list_normalization_parameters, pretty = T),
-      path = "data/processed/logistic_model_parameters.json"
-    )
-
     dimorphism_model_albatross <- dimorphism_model$new()
-    dimorphism_model_albatross$load_parameters("data/processed/logistic_model_parameters.json")
+    dimorphism_model_albatross$load_parameters("data/processed/best_logistic_model_parameters_laal_ig.json")
     prob <- dimorphism_model_albatross$predict(data)
     males <- append(males, as.logical(prob > threshold))
   }
