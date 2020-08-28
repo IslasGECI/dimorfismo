d0eef26ee54dcd4f49e1cb998bf8ed638ecbbeaa
Andrea <a348644@uabc.edu.mx>
2020-08-28 00:29:55 -0700

    Modifica dirreción de variable

--- src/03_predict_sex.R
+++ src/03_predict_sex.R
@@ -5,7 +5,7 @@ tdp_path <- ("data/raw/")
 csv_file <- file.path(tdp_path, "laysan_albatross_morphometry_guadalupe.csv")
 csv_data <- data.table::data.table(read.csv(csv_file))
 results_path <- "data/processed/"
-json_file <- "data/processed/best_logistic_model_parameters_laal_ig.json"
+json_file <- file.path(results_path, "best_logistic_model_parameters_laal_ig.json")
 imported_table <- data.table::data.table(
   readr::read_csv(paste0(results_path, "best_models_table.csv"))
 )
