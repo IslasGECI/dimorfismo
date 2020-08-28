c46b86769d7d5220ae421b0ed14d45e95b725d2a
Andrea <a348644@uabc.edu.mx>
2020-08-27 23:22:13 -0700

    Aplica guía de estilo en 03_predict_sex.R

--- src/03_predict_sex.R
+++ src/03_predict_sex.R
@@ -5,21 +5,21 @@ tdp_path <- ("data/raw/")
 csv_file <- file.path(tdp_path, "laysan_albatross_morphometry_guadalupe.csv")
 csv_data <- data.table::data.table(read.csv(csv_file))
 results_path <- "data/processed/"
+json_file <- "data/processed/best_logistic_model_parameters_laal_ig.json"
 imported_table <- data.table::data.table(
   readr::read_csv(paste0(results_path, "best_models_table.csv"))
 )
 
 n_rows_table <- nrow(imported_table)
 n_rows_data <- nrow(csv_data)
-males <- c()
-
 threshold <- as.numeric(imported_table[1, 7])
+males <- c()
 
 for (i_albatross in 1:n_rows_data) {
   data <- csv_data[i_albatross, ]
   for (i_row in 1:n_rows_table) {
     dimorphism_model_albatross <- dimorphism_model$new()
-    dimorphism_model_albatross$load_parameters("data/processed/best_logistic_model_parameters_laal_ig.json")
+    dimorphism_model_albatross$load_parameters(json_file)
     prob <- dimorphism_model_albatross$predict(data)
     males <- append(males, as.logical(prob > threshold))
   }
