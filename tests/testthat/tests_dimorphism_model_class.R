0d4b5108348342dae71b5098d6e0641ca40abb6a
Andrea <a348644@uabc.edu.mx>
2020-10-28 13:20:01 -0700

    :construction: :white_check_mark: Agrega prueba del método load_paramete

--- tests/testthat/tests_dimorphism_model_class.R
+++ tests/testthat/tests_dimorphism_model_class.R
@@ -1,3 +1,21 @@
 library(data.table)
 library(dimorfismo)
 library(testthat)
+
+get_private <- function(object) {
+  object[['.__enclos_env__']]$private
+}
+
+setwd("/workdir/")
+json_correct_data_path <- "tests/data/best_logistic_model_parameters_laal_ig_tests.json"
+json_data_path <- "data/processed/best_logistic_model_parameters_laal_ig.json"
+
+dimorphism_class_tester <- dimorphism_model$new()
+
+test_that("El método load_parameters funciona: ", {
+  dimorphism_class_tester$load_parameters(json_data_path)
+  model_parameters <- get_private(dimorphism_class_tester)$model_parameters
+  dimorphism_class_tester$load_parameters(json_correct_data_path)
+  correct_model_parameters <- get_private(dimorphism_class_tester)$model_parameters
+  expect_equal(model_parameters, correct_model_parameters)
+})
