cc410b3a22b7b51ec6ab290bc367619313052fd7
Andrea <a348644@uabc.edu.mx>
2020-11-04 18:53:40 -0800

    :white_check_mark: Agrega prueba para función privada get_value

--- tests/testthat/tests_dimorphism_model_class.R
+++ tests/testthat/tests_dimorphism_model_class.R
@@ -12,6 +12,7 @@ dimorphism_class_expected <- dimorphism_model$new()
 
 json_correct_data_path <- "tests/data/best_logistic_model_parameters_laal_ig_tests.json"
 json_data_path <- "data/processed/best_logistic_model_parameters_laal_ig.json"
+
 test_that("El método load_parameters funciona: ", {
   correct_model_parameters <- dimorphism_class_expected$load_parameters(json_correct_data_path)
   model_parameters <- dimorphism_class_tester$load_parameters(json_data_path)
@@ -25,9 +26,18 @@ csv_data <- data.table(read.csv(csv_file))
 data_training <- csv_data[1:5, ]
 
 test_that("El método predict funciona: ", {
-  expected_prediction <- data.frame(
+  correct_prediction <- data.frame(
   "probability" = c(0.9916232780, 0.0005606211, 0.9904448866, 0.2978475227, 0.9617133474)
   )
   prediction <- dimorphism_class_tester$predict(data_training)
-  expect_equivalent(prediction, expected_prediction)
+  expect_equivalent(prediction, correct_prediction)
+})
+
+test_that("La función privada get_value funciona: ", {
+  correct_estimate_peak <- 6.648
+  correct_intercept <- -12.87
+  estimate_intercept <- get_private(dimorphism_class_tester)$get_value("(Intercept)")
+  estimate_peak <- get_private(dimorphism_class_tester)$get_value("longitud_pico")
+  expect_equal(estimate_intercept, correct_intercept)
+  expect_equal(estimate_peak, correct_estimate_peak)
 })
