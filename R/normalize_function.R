8acf62171c20f4a2c2f0f56be94a7059a414b0e5
Andrea <a348644@uabc.edu.mx>
2020-11-05 14:17:06 -0800

    :recycle: Elimina return de la función normalize

--- R/normalize_function.R
+++ R/normalize_function.R
@@ -1,5 +1,4 @@
 #' @export
 normalize <- function(column, minimum = min(column), maximum = max(column)) {
   normalize_return <- (column - minimum) / (maximum - minimum)
-  return(normalize_return)
 }
