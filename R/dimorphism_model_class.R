1903096784de9c139a2535f361533cb75ff9803c
Andrea <a348644@uabc.edu.mx>
2020-11-17 15:57:19 -0800

    :zap: Cambia a zeta como variable privada

--- R/dimorphism_model_class.R
+++ R/dimorphism_model_class.R
@@ -10,8 +10,8 @@ dimorphism_model <- R6Class("dimorphism_model",
     },
 
     predict = function(morphometric_data_table) {
-      z <- private$get_z_value(morphometric_data_table)
-      probability <- 1 / (1 + exp(-z))
+      private$get_z_value(morphometric_data_table)
+      probability <- 1 / (1 + exp(- private$z))
       colnames(probability) <- "probability"
       return(probability)
     }
@@ -20,52 +20,55 @@ dimorphism_model <- R6Class("dimorphism_model",
   private = list(
     model_parameters = NULL,
     normalization_parameters = NULL,
-
-    calculate_z = function(data_table, variable, z) {
-      column <- data_table[, variable$Variables]
-      normalized_column <- private$normalize_column(column, variable)
-      z <- z + normalized_column * private$get_estimate_value(variable$Variables)
-      return(z)
-    },
-
-    get_estimate_value = function(variable_name) {
-      for (variable in private$model_parameters) {
-        if (variable$Variables == variable_name) {
-          return(variable$Estimate)
-        }
-      }
-    },
-
-    get_maximum = function(variable) {
-      maximum <- as.numeric(private$normalization_parameters$maximum_value[variable$Variables])
-      return(maximum)
-    },
-
-    get_minimum = function(variable) {
-      minimum <- as.numeric(private$normalization_parameters$minimum_value[variable$Variables])
-      return(minimum)
-    },
+    z = NULL,
 
     get_z_value = function(data_table) {
-      z <- private$get_estimate_value("(Intercept)")
+      private$z <- private$get_estimate_value("(Intercept)")
       morphometric_parameters <- private$remove_intercept()
       for (variable in morphometric_parameters) {
-        z <- private$calculate_z(data_table, variable, z)
+        private$z <- private$calculate_z(data_table, variable)
       }
-      return(z)
     },
 
-    normalize_column = function(column, variable) {
-      minimum <- private$get_minimum(variable)
-      maximum <- private$get_maximum(variable)
-      normalized_column <- as.data.frame(normalize(column, minimum, maximum))
-      return(normalized_column)
-    },
+      get_estimate_value = function(variable_name) {
+        for (variable in private$model_parameters) {
+          if (variable$Variables == variable_name) {
+            return(variable$Estimate)
+          }
+        }
+      },
 
-    remove_intercept = function() {
-      n_parameters <- length(private$model_parameters)
-      morphometric_parameters <- private$model_parameters[2:n_parameters]
-      return(morphometric_parameters)
-    }
+      calculate_z = function(data_table, variable) {
+        column <- data_table[, variable$Variables]
+        normalized_column <- private$normalize_column(column, variable)
+        private$z <- private$z + normalized_column * private$get_estimate_value(variable$Variables)
+      },
+
+        normalize_column = function(column, variable) {
+          minimum <- private$get_minimum(variable)
+          maximum <- private$get_maximum(variable)
+          normalized_column <- as.data.frame(normalize(column, minimum, maximum))
+          return(normalized_column)
+        },
+
+          get_maximum = function(variable) {
+            maximum <- as.numeric(
+              private$normalization_parameters$maximum_value[variable$Variables]
+            )
+            return(maximum)
+          },
+
+          get_minimum = function(variable) {
+            minimum <- as.numeric(
+              private$normalization_parameters$minimum_value[variable$Variables]
+            )
+            return(minimum)
+          },
+
+      remove_intercept = function() {
+        n_parameters <- length(private$model_parameters)
+        morphometric_parameters <- private$model_parameters[2:n_parameters]
+        return(morphometric_parameters)
+      }
   )
 )
