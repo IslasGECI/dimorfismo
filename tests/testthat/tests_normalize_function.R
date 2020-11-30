fc1bb8ae7c258c5a101ad0f02208e038bd1708a0
Andrea <a348644@uabc.edu.mx>
2020-11-30 15:36:47 -0800

    :pencil2: Acomoda variables por orden de aparición

--- tests/testthat/tests_normalize_function.R
+++ tests/testthat/tests_normalize_function.R
@@ -1,14 +1,14 @@
 library(dimorfismo)
 library(testthat)
 
-test_data <- c(
-    100, 105, 200, 250, 300, 350
-)
-min_test_data <- 100
-max_test_data <- 350
-normalized_data <- c(0, 0.02, 0.4, 0.6, 0.8, 1)
+expected_result <- c(0, 0.02, 0.4, 0.6, 0.8, 1)
+test_data <- c(100, 105, 200, 250, 300, 350)
 
-test_that("La función normalize_function funciona: ", {
-    expect_equal(normalized_data, normalize(test_data))
-    expect_equal(normalized_data, normalize(test_data, min_test_data, max_test_data))
+test_that("La función normalize_function funciona correctamente: ", {
+    data_to_test <- normalize(test_data)
+    expect_equal(expected_result, data_to_test)
+    max_test_data <- 350
+    min_test_data <- 100
+    limited_data_to_test <- normalize(test_data, min_test_data, max_test_data)
+    expect_equal(expected_result, limited_data_to_test)
 })
