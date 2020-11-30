98f00fa988aaf8074a0cb3ece36d05f87d42c5f5
Andrea <a348644@uabc.edu.mx>
2020-11-30 15:22:46 -0800

    :white_check_mark: Agrega segunda prueba de normalizec_function

--- tests/testthat/tests_normalize_function.R
+++ tests/testthat/tests_normalize_function.R
@@ -4,9 +4,11 @@ library(testthat)
 test_data <- c(
     100, 105, 200, 250, 300, 350
 )
-
+min_test_data <- 100
+max_test_data <- 350
 normalized_data <- c(0, 0.02, 0.4, 0.6, 0.8, 1)
 
 test_that("La función normalize_function funciona: ", {
     expect_equal(normalized_data, normalize(test_data))
+    expect_equal(normalized_data, normalize(test_data, min_test_data, max_test_data))
 })
