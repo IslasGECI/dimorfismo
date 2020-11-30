c4321e14f7d02d76c17a58e9054390560d7eb0df
Andrea <a348644@uabc.edu.mx>
2020-11-30 15:01:09 -0800

    :white_check_mark: Agrega primer prueba de normalized_function

--- tests/testthat/tests_normalize_function.R
+++ tests/testthat/tests_normalize_function.R
@@ -1,2 +1,12 @@
 library(dimorfismo)
 library(testthat)
+
+test_data <- c(
+    100, 105, 200, 250, 300, 350
+)
+
+normalized_data <- c(0, 0.02, 0.4, 0.6, 0.8, 1)
+
+test_that("La función normalize_function funciona: ", {
+    expect_equal(normalized_data, normalize(test_data))
+})
