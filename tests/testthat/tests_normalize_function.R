442fd6e2d25d8cea12885c144431c565a6f1571e
Andrea <a348644@uabc.edu.mx>
2020-12-01 12:33:00 -0800

    ⚡️ Cambia a max y min de manera explicita
    Co-authored-by: nepito <braulio.rojas@islas.org.mx>

--- tests/testthat/tests_normalize_function.R
+++ tests/testthat/tests_normalize_function.R
@@ -7,8 +7,8 @@ test_data <- c(100, 105, 200, 250, 300, 350)
 test_that("La función normalize_function funciona correctamente: ", {
     data_to_test <- normalize(test_data)
     expect_equal(expected_result, data_to_test)
-    max_test_data <- 350
-    min_test_data <- 100
+    max_test_data <- max(test_data)
+    min_test_data <- min(test_data)
     limited_data_to_test <- normalize(test_data, min_test_data, max_test_data)
     expect_equal(expected_result, limited_data_to_test)
 })
