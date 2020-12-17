8909a05a11a48e2b27314c4e6ab5d0cf607cb710
Andrea <a348644@uabc.edu.mx>
2020-12-16 16:00:05 -0800

    ✅  Agrega descripción en prueba

--- tests/testthat/tests_normalize_function.R
+++ tests/testthat/tests_normalize_function.R
@@ -4,11 +4,15 @@ library(testthat)
 expected_result <- c(0, 0.02, 0.4, 0.6, 0.8, 1)
 test_data <- c(100, 105, 200, 250, 300, 350)
 
-test_that("La función normalize_function funciona correctamente: ", {
-    data_to_test <- normalize(test_data)
-    expect_equal(expected_result, data_to_test)
-    max_test_data <- max(test_data)
-    min_test_data <- min(test_data)
-    limited_data_to_test <- normalize(test_data, min_test_data, max_test_data)
-    expect_equal(expected_result, limited_data_to_test)
+describe("La función normalize_function funciona correctamente: ", {
+    it("Caso 1: ", {
+        data_to_test <- normalize(test_data)
+        expect_equal(expected_result, data_to_test)
+    })
+    it("Caso 2: Incluyendo variables de entrada max y min", {
+        max_test_data <- max(test_data)
+        min_test_data <- min(test_data)
+        limited_data_to_test <- normalize(test_data, min_test_data, max_test_data)
+        expect_equal(expected_result, limited_data_to_test)
+    })
 })
