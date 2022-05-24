6ef0ea59074079560cb7487d918d8d64c563125a
Alejandro Padilla <alejandro.padilla@islas.org.mx>
2022-05-24 09:14:43 -0700

    🚧 Add new test
    Se añadió una nueva prueba para el material didactico.

--- tests/testthat/tests_regretion_to_data_frame_coefficients_function.R
+++ tests/testthat/tests_regretion_to_data_frame_coefficients_function.R
@@ -61,12 +61,6 @@ test_that("Logistic regretion from scratch", {
   expect_equal(expected, obtained)
 })
 
-sintetic_data <- tibble(
-  sexo = c(0.0009, 0.0024, 0.0067, 0.018, 0.047, 0.119, 0.269, 0.500, 0.731, 0.881, 0.953, 0.982, 0.993, 0.998, 0.999, 1, 1, 1, 1, 1),
-  x = seq(-2, 1.8, 0.2)
-)
-sintetic_data2 <- sintetic_data %>% mutate(sexo = rbinom(20, 1, sintetic_data$sexo))
-
 test_that("Logistic function", {
   expected <- 0.953
   obtained <- logt(0)
