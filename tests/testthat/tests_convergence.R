c5c59947d3353c502ed94b3f130c3666f19e3ff8
Alejandro Padilla <alejandro.padilla@islas.org.mx>
2022-05-24 09:22:57 -0700

    ⚗️ Add dependencies
    Se agregaron dependencias

--- tests/testthat/tests_convergence.R
+++ tests/testthat/tests_convergence.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(dimorfismo)
+library(testthat)
 
 sintetic_data <- tibble(
   sexo = c(0.0009, 0.0024, 0.0067, 0.018, 0.047, 0.119, 0.269, 0.500, 0.731, 0.881, 0.953, 0.982, 0.993, 0.998, 0.999, 1, 1, 1, 1, 1),
